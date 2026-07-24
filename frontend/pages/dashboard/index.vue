cebd3134d60ce92f90aac98c8b2140a618bc86bb
monoland <mono.socialmedia@gmail.com>
2026-07-24 09:08:46 +0700

    feat: render dashboard KPI widgets

--- frontend/pages/dashboard/index.vue
+++ frontend/pages/dashboard/index.vue
@@ -1,6 +1,44 @@
 <template>
 	<page-home page-name="training-dashboard">
-		<template v-slot:default></template>
+		<template v-slot:default="{ record }">
+			<v-row dense>
+				<v-col md="3" cols="12">
+					<widget-number
+						color="teal"
+						icon="school"
+						label="Total Kegiatan Pelatihan"
+						:value="`${record.totalEvents ?? 0}`"
+					></widget-number>
+				</v-col>
+
+				<v-col md="3" cols="12">
+					<widget-number
+						color="orange"
+						icon="event_upcoming"
+						label="Kegiatan Akan Datang"
+						:value="`${record.upcomingEvents ?? 0}`"
+					></widget-number>
+				</v-col>
+
+				<v-col md="3" cols="12">
+					<widget-number
+						color="green"
+						icon="task_alt"
+						label="Kegiatan Selesai"
+						:value="`${record.completedEvents ?? 0}`"
+					></widget-number>
+				</v-col>
+
+				<v-col md="3" cols="12">
+					<widget-number
+						color="blue"
+						icon="how_to_reg"
+						label="Total Peserta Diterima"
+						:value="`${record.acceptedParticipants ?? 0}`"
+					></widget-number>
+				</v-col>
+			</v-row>
+		</template>
 	</page-home>
 </template>
 
